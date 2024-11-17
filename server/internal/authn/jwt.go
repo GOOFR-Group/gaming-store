078a1c3f0629d7fe87751874e9ff500014f2d2c2
Goncalo-Marques <53789763+Goncalo-Marques@users.noreply.github.com>
2024-11-17 17:41:10 +0000

    feat: add authn

---
+++ server/internal/authn/jwt.go
@@ -0,0 +1,66 @@
+package authn
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/golang-jwt/jwt/v5"
+)
+
+const (
+	jwtIssuer         = "server"
+	jwtExpirationTime = time.Hour * 24
+
+	descriptionFailedToParseJWTWithClaims = "authn: failed to parse jwt with claims"
+)
+
+// SubjectRole defines the role of the subject.
+type SubjectRole string
+
+const (
+	SubjectRoleUser      SubjectRole = "user"
+	SubjectRolePublisher SubjectRole = "publisher"
+)
+
+// Claims defines the JSON Web Token claims structure.
+type Claims struct {
+	jwt.RegisteredClaims
+	Roles []SubjectRole `json:"roles,omitempty"`
+}
+
+// NewJWT returns a new signed JSON Web Token with an expiration time of 24 hours and the specified claims.
+func (s *service) NewJWT(subject string, subjectRoles []SubjectRole) (string, error) {
+	expiresAt := time.Now().Add(jwtExpirationTime).UTC()
+	issuedAt := time.Now().UTC()
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, Claims{
+		RegisteredClaims: jwt.RegisteredClaims{
+			Issuer:    jwtIssuer,
+			Subject:   subject,
+			ExpiresAt: jwt.NewNumericDate(expiresAt),
+			IssuedAt:  jwt.NewNumericDate(issuedAt),
+		},
+		Roles: subjectRoles,
+	})
+
+	tokenString, err := token.SignedString(s.jwtSigningKey)
+	if err != nil {
+		return "", err
+	}
+
+	return tokenString, nil
+}
+
+// ParseJWT parses the given token and returns the associated subject, or returns an error if the token is invalid.
+func (s *service) ParseJWT(tokenString string) (Claims, error) {
+	var claims Claims
+
+	_, err := jwt.ParseWithClaims(tokenString, &claims, func(t *jwt.Token) (interface{}, error) {
+		return s.jwtSigningKey, nil
+	})
+	if err != nil {
+		return Claims{}, fmt.Errorf("%s: %w", descriptionFailedToParseJWTWithClaims, err)
+	}
+
+	return claims, nil
+}
---
+++ server/internal/authn/password.go
@@ -0,0 +1,66 @@
+package authn
+
+import (
+	"errors"
+	"unicode"
+
+	"golang.org/x/crypto/bcrypt"
+)
+
+const (
+	passwordMinLength       = 14
+	passwordMaxLength       = 72
+	passwordMinDigits       = 1
+	passwordMinChars        = 1
+	passwordMinSpecialChars = 1
+)
+
+// ValidPassword returns true if the password matches the requirements, false otherwise.
+func (s *service) ValidPassword(password []byte) bool {
+	// Check password length requirements.
+	if len(password) < passwordMinLength || len(password) > passwordMaxLength {
+		return false
+	}
+
+	// Check password complexity requirements.
+	var (
+		digits       int
+		chars        int
+		specialChars int
+	)
+
+	for _, r := range string(password) {
+		if unicode.IsDigit(r) {
+			digits++
+			continue
+		}
+
+		if unicode.IsLetter(r) {
+			chars++
+		} else {
+			specialChars++
+		}
+	}
+
+	return digits >= passwordMinDigits && chars >= passwordMinChars && specialChars >= passwordMinSpecialChars
+}
+
+// HashPassword returns the hashed password.
+func (s *service) HashPassword(password []byte) ([]byte, error) {
+	bytes, err := bcrypt.GenerateFromPassword(password, bcrypt.DefaultCost)
+	return bytes, err
+}
+
+// CheckPasswordHash returns true if the password matches the hashed password, false otherwise.
+func (s *service) CheckPasswordHash(password, hash []byte) (bool, error) {
+	err := bcrypt.CompareHashAndPassword(hash, password)
+	if err != nil {
+		if errors.Is(err, bcrypt.ErrMismatchedHashAndPassword) {
+			return false, nil
+		}
+
+		return false, err
+	}
+
+	return true, nil
+}
