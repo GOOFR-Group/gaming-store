8f9246dcb2aeac725ddffa75299cc8a8a5ae4697
Goncalo-Marques <53789763+Goncalo-Marques@users.noreply.github.com>
2024-11-17 17:42:19 +0000

    feat: add domain

---
+++ server/internal/domain/fields.go
@@ -0,0 +1,15 @@
+package domain
+
+// Field names.
+const (
+	FieldParamUserID      = "userId"
+	FieldParamPublisherID = "publisherId"
+
+	FieldUsername    = "username"
+	FieldEmail       = "email"
+	FieldPassword    = "password"
+	FieldDisplayName = "displayName"
+	FieldAddress     = "address"
+	FieldCountry     = "country"
+	FieldVatin       = "vatin"
+)
