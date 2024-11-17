04ec33e6929fc9e58d8e9e43d767103722e608f0
Goncalo-Marques <53789763+Goncalo-Marques@users.noreply.github.com>
2024-11-17 17:40:32 +0000

    feat: add logging keys

---
+++ server/internal/logging/keys.go
@@ -0,0 +1,18 @@
+package logging
+
+// Attribute key const.
+const (
+	BuildGitHash   = "build.gitHash"
+	BuildTimestamp = "build.timestamp"
+	HostName       = "host.name"
+
+	ServiceMethod = "service.method"
+
+	UserUsername    = "user.username"
+	UserEmail       = "user.email"
+	UserDisplayName = "user.displayName"
+	UserDateOfBirth = "user.dateOfBirth"
+	UserAddress     = "user.address"
+	UserCountry     = "user.country"
+	UserVatin       = "user.vatin"
+)
