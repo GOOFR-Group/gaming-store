4b97e45295008be06356c3cf8ebb5f438d94c9e3
Goncalo-Marques <53789763+Goncalo-Marques@users.noreply.github.com>
2024-11-18 10:07:16 +0000

    feat: update spec with multimedia

--- server/internal/transport/http/multimedia.go
+++ server/internal/transport/http/multimedia.go
@@ -1,7 +1,14 @@
 package http
 
+import "net/http"
+
 const (
 	codeMultimediaNotFound = "multimedia_not_found"
 
 	errMultimediaNotFound = "multimedia not found"
 )
+
+// UploadMultimedia handles the http request to upload multimedia.
+func (h *handler) UploadMultimedia(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusNotImplemented)
+}
