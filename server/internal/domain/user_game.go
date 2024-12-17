7d91281ed1aaf7f29f477444cdde3b492ebac822
Goncalo-Marques <53789763+Goncalo-Marques@users.noreply.github.com>
2024-12-17 13:40:31 +0000

    feat: add domain

--- server/internal/domain/user_game.go
+++ server/internal/domain/user_game.go
@@ -1,5 +1,34 @@
 package domain
 
+// UserCartGamePaginatedSort defines the field of the user cart game to sort.
+type UserCartGamePaginatedSort string
+
+const (
+	UserCartGamePaginatedSortCreatedAt UserCartGamePaginatedSort = "createdAt"
+	UserCartGamePaginatedSortGameTitle UserCartGamePaginatedSort = "gameTitle"
+)
+
+// Field returns the name of the field to sort by.
+func (s UserCartGamePaginatedSort) Field() UserCartGamePaginatedSort {
+	return s
+}
+
+// Valid returns true if the field is valid, false otherwise.
+func (s UserCartGamePaginatedSort) Valid() bool {
+	switch s {
+	case UserCartGamePaginatedSortCreatedAt,
+		UserCartGamePaginatedSortGameTitle:
+		return true
+	default:
+		return false
+	}
+}
+
+// UserCartGamesPaginatedFilter defines the user cart games filter structure.
+type UserCartGamesPaginatedFilter struct {
+	PaginatedRequest[UserCartGamePaginatedSort]
+}
+
 // UserLibraryGamePaginatedSort defines the field of the user library game to sort.
 type UserLibraryGamePaginatedSort string
 
