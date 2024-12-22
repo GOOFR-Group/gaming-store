e9fab4b818bb937fae1a342f91566610f3667782
joaotomaspinheiro <joaotomasp@gmail.com>
2024-12-22 23:19:58 +0000

    fix: lint

--- web/src/routes/distribute/_layout/account.tsx
+++ web/src/routes/distribute/_layout/account.tsx
@@ -1,19 +1,19 @@
-import { queryOptions, useSuspenseQuery } from '@tanstack/react-query'
-import { createFileRoute } from '@tanstack/react-router'
+import { queryOptions, useSuspenseQuery } from "@tanstack/react-query";
+import { createFileRoute } from "@tanstack/react-router";
 
-import { PublisherAccountDetails } from '@/components/distribute/account/account-details'
-import { PublisherAvatar } from '@/components/distribute/account/publisher-avatar'
+import { PublisherAccountDetails } from "@/components/distribute/account/account-details";
+import { PublisherAvatar } from "@/components/distribute/account/publisher-avatar";
 import {
   Card,
   CardContent,
   CardDescription,
   CardHeader,
   CardTitle,
-} from '@/components/ui/card'
-import { getPublisher } from '@/lib/api'
-import { decodeTokenPayload, getToken } from '@/lib/auth'
-import { COUNTRIES_MAP, MISSING_VALUE_SYMBOL } from '@/lib/constants'
-import { publisherQueryKey } from '@/lib/query-keys'
+} from "@/components/ui/card";
+import { getPublisher } from "@/lib/api";
+import { decodeTokenPayload, getToken } from "@/lib/auth";
+import { COUNTRIES_MAP, MISSING_VALUE_SYMBOL } from "@/lib/constants";
+import { publisherQueryKey } from "@/lib/query-keys";
 
 /**
  * Query options for retrieving the signed in publisher.
@@ -23,30 +23,30 @@ function publisherQueryOptions() {
   return queryOptions({
     queryKey: publisherQueryKey,
     async queryFn() {
-      const token = getToken()
-      const payload = decodeTokenPayload(token)
+      const token = getToken();
+      const payload = decodeTokenPayload(token);
 
-      const publisherId = payload.sub
-      const publisher = await getPublisher(publisherId)
+      const publisherId = payload.sub;
+      const publisher = await getPublisher(publisherId);
 
-      return publisher
+      return publisher;
     },
-  })
+  });
 }
 
-export const Route = createFileRoute('/distribute/_layout/account')({
+export const Route = createFileRoute("/distribute/_layout/account")({
   component: Component,
   loader(opts) {
-    return opts.context.queryClient.ensureQueryData(publisherQueryOptions())
+    return opts.context.queryClient.ensureQueryData(publisherQueryOptions());
   },
-})
+});
 
 function Component() {
-  const query = useSuspenseQuery(publisherQueryOptions())
-  const publisher = query.data
+  const query = useSuspenseQuery(publisherQueryOptions());
+  const publisher = query.data;
   const country =
     COUNTRIES_MAP[publisher.country.toUpperCase() as keyof typeof COUNTRIES_MAP]
-      ?.name ?? MISSING_VALUE_SYMBOL
+      ?.name ?? MISSING_VALUE_SYMBOL;
 
   return (
     <Card className="flex flex-col h-full">
@@ -67,5 +67,5 @@ function Component() {
         <PublisherAccountDetails country={country} publisher={publisher} />
       </CardContent>
     </Card>
-  )
+  );
 }
