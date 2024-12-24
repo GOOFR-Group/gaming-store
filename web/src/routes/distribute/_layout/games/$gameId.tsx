278f5d1fb50d413504458ecb532b7df582681751
Goncalo-Marques <53789763+Goncalo-Marques@users.noreply.github.com>
2024-12-24 12:21:54 +0000

    feat: remove redundant information

--- web/src/routes/distribute/_layout/games/$gameId.tsx
+++ web/src/routes/distribute/_layout/games/$gameId.tsx
@@ -7,13 +7,7 @@ import { ErrorPage } from "@/components/distribute/error";
 import { GamePreview } from "@/components/distribute/games/game-preview";
 import { Badge } from "@/components/ui/badge";
 import { Button } from "@/components/ui/button";
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import {
   Tooltip,
   TooltipContent,
@@ -82,16 +76,13 @@ function Component() {
     <Card className="flex flex-col min-h-full">
       <CardHeader className="flex flex-wrap flex-row gap-2 justify-between">
         <div className="flex flex-col gap-1">
-          <div className="flex items-center gap-4">
-            <CardTitle>{game.title}</CardTitle>
-            <Badge
-              className="w-fit"
-              variant={game.isActive ? "default" : "destructive"}
-            >
-              {game.isActive ? "Active" : "Inactive"}
-            </Badge>
-          </div>
-          <CardDescription>{game.publisher.name}</CardDescription>
+          <CardTitle>{game.title}</CardTitle>
+          <Badge
+            className="w-fit"
+            variant={game.isActive ? "default" : "destructive"}
+          >
+            {game.isActive ? "Active" : "Inactive"}
+          </Badge>
         </div>
         <div className="flex-1 flex gap-2 first:*:ml-auto">
           {game.downloadMultimedia ? (
