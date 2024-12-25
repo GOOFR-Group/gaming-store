d94c4ce0aea7446683d31f742de6d68984b678f8
Goncalo-Marques <53789763+Goncalo-Marques@users.noreply.github.com>
2024-12-25 14:25:53 +0000

    feat: add mb to component

--- web/src/components/underConstruction.tsx
+++ web/src/components/underConstruction.tsx
@@ -8,9 +8,11 @@ export function UnderConstructionPage(props: { showBack?: boolean }) {
   return (
     <Card className="flex flex-col items-center text-center min-h-full">
       <CardTitle className="mt-12">Under Construction</CardTitle>
-      <CardDescription>This page is under construction.</CardDescription>
+      <CardDescription className={props.showBack ? "" : "mb-12"}>
+        This page is under construction.
+      </CardDescription>
       {props.showBack && (
-        <Button asChild className="w-fit mt-4">
+        <Button asChild className="w-fit mt-4 mb-12">
           <Link to="..">
             <ArrowLeft /> Go Back
           </Link>
