a8ed26710ff1acf4be193cf83f140ea3266dcaaf
joaotomaspinheiro <joaotomasp@gmail.com>
2024-12-29 18:20:58 +0000

    fix: game key

--- web/src/routes/_layout/index.tsx
+++ web/src/routes/_layout/index.tsx
@@ -235,7 +235,7 @@ function Section(props: {
         <div className="grid grid-cols-2 md:grid-cols-4 lg:grid-cols-6 gap-6 *:mx-auto">
           {props.games.map((game) => (
             <Link
-              key={game.title}
+              key={game.id}
               params={{ gameId: game.id, publisherId: game.publisher.id }}
               to="/publishers/$publisherId/games/$gameId"
             >
